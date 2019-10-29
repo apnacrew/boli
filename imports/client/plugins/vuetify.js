997f8c1907b1babc05f17b78626f9f47e9589911
Lavneet <lavneetpuri@gmail.com>
2019-10-29 09:37:29 +0000

    removed unsed vuetify options

--- imports/client/plugins/vuetify.js
+++ imports/client/plugins/vuetify.js
@@ -4,8 +4,4 @@ import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-  icons: {
-    iconfont: 'fa', // default - only for display purposes
-  },
-});
+export default new Vuetify();
