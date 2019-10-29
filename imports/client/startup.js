3842db0e999bccb6cf2df30b1671bda5f74d4d4a
Lavneet <lavneetpuri@gmail.com>
2019-10-29 05:51:51 +0000

    added material icon fonts

--- imports/client/startup.js
+++ imports/client/startup.js
@@ -1,2 +1,6 @@
-// Load all plugins
+/** Load all plugins */
 import './plugins/keen-ui';
+
+/** Load stylesheets: each import will appear as seperate style tag */
+// Fonts: make sure referenced fonts are present in /public/fonts directory
+import 'material-icons-font/material-icons-font.css';
