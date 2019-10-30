2393314593fa698fe17ce31fa87f80fb8df983f2
Lavneet <lavneetpuri@gmail.com>
2019-10-30 00:03:11 +0000

    use font-awesome icons instead of material because of variety and shape options

--- imports/client/startup.js
+++ imports/client/startup.js
@@ -1,3 +1,3 @@
 /** Load stylesheets: each import will appear as seperate style tag */
-// Fonts: make sure referenced fonts are present in /public/fonts directory
-import 'material-icons-font/material-icons-font.css';
+// Fonts: make sure referenced fonts are present in /public directory
+import '@fortawesome/fontawesome-free/css/all.css';
