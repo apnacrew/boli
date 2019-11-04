a7f69f604f884d1f97876450a8e456c2038e9e9c
Lavneet <lavneetpuri@gmail.com>
2019-11-04 06:06:22 +0000

    i1-update song document format

--- imports/server/startup.js
+++ imports/server/startup.js
@@ -5,11 +5,21 @@ Songs.upsert({
   name: 'Qismat',
   artist: 'Ammy Virk',
   writer: 'Jaani',
-  runtime: 643,
+  runtime: 243,
   release: new Date('22 Jul 2017'),
 }, {
   $set: {
     description: 'Qismat is a punjabi romanticized song reflecting the scarifices we make in love in order to win a perpetual fight against our destiny.',
     imageUrl: '/images/qismat-cover.jpg',
+    lyrics: {
+      orig: {
+        lang: 'Punjabi',
+        lines: [],
+      },
+      translated: {
+        lang: 'English',
+        lines: [],
+      },
+    },
   },
 });
