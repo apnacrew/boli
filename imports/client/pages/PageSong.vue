8c0c3675b57dbb4858e7070cd82bd361a0d1c235
Lavneet <lavneetpuri@gmail.com>
2019-11-04 06:11:19 +0000

    i1-update song page template to use data from song collection

--- imports/client/pages/PageSong.vue
+++ imports/client/pages/PageSong.vue
@@ -1,64 +1,154 @@
 <template>
   <v-container fluid>
-    <v-row>
-      <v-col>
-        <v-card flat>
-          <v-img
-            src="/images/kismet-cover.jpg"
-            height="200px"
-          >
-            <v-card-title class="display-2 font-weight-bold">
-              Top western road trips
-            </v-card-title>
-            <v-card-subtitle class="title font-weight-bold">
-              By Artist Name
-            </v-card-subtitle>
-          </v-img>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-card flat>
-          <v-card-title class="headline font-weight-bold">
-            Lyrics
-          </v-card-title>
-          <v-card-text class="pb-0">
-            <v-row>
-              <v-col>
-                <v-card flat>
-                  <div class="font-weight-bold">
-                    Punjabi
-                  </div>
-                  <div class="lyrics-wrapper">
-                    {{ loremIpsum({ count: 10 }) }}
-                  </div>
-                </v-card>
-              </v-col>
-              <v-divider vertical />
-              <v-col>
-                <v-card flat>
-                  <div class="font-weight-bold">
-                    English
-                  </div>
-                  <div class="lyrics-wrapper">
-                    {{ loremIpsum({ count: 10 }) }}
-                  </div>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-card
+      tile
+      flat
+      :loading="loading"
+    >
+      <v-alert
+        v-if="error"
+        type="error"
+      >
+        We were unable to fetch the requested song. Please make sure the url is correct and try again.
+      </v-alert>
+      <!-- Song profile -->
+      <template v-else>
+        <v-row>
+          <v-col>
+            <v-card
+              flat
+              tile
+            >
+              <v-img
+                :src="song.imageUrl"
+                height="200px"
+              >
+                <v-card-title class="display-2 font-weight-bold">
+                  {{ song.name }}
+                </v-card-title>
+                <v-card-subtitle class="title font-weight-bold">
+                  {{ song.artist }}
+                </v-card-subtitle>
+              </v-img>
+              <v-card-text>
+                <div>
+                  {{ song.description }}
+                </div>
+                <v-row
+                  class="meta flex-column"
+                  wrap
+                  dense
+                  style="max-height: 70px"
+                >
+                  <v-col sm="2">
+                    <span class="font-weight-bold">Runtime: </span>
+                    {{ runtime }}
+                  </v-col>
+                  <v-col sm="2">
+                    <span class="font-weight-bold">Release Date: </span>
+                    {{ releaseDate }}
+                  </v-col>
+                  <v-col sm="2">
+                    <span class="font-weight-bold">Writer: </span>
+                    {{ song.writer }}
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card flat>
+              <v-card-title class="headline font-weight-bold">
+                Lyrics
+              </v-card-title>
+              <v-card-text class="pb-0">
+                <v-row>
+                  <v-col>
+                    <v-card flat>
+                      <div class="font-weight-bold">
+                        {{ song.lyrics.orig.lang }}
+                      </div>
+                      <div class="lyrics-wrapper">
+                        {{ loremIpsum({ count: 10 }) }}
+                      </div>
+                    </v-card>
+                  </v-col>
+                  <v-divider vertical />
+                  <v-col>
+                    <v-card flat>
+                      <div class="font-weight-bold">
+                        {{ song.lyrics.translated.lang }}
+                      </div>
+                      <div class="lyrics-wrapper">
+                        {{ loremIpsum({ count: 10 }) }}
+                      </div>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+    </v-card>
   </v-container>
 </template>
 
 <script>
+import { Meteor } from 'meteor/meteor';
+import moment from 'moment';
 import { loremIpsum } from 'lorem-ipsum';
 
 export default {
   name: 'PageSong',
+  data() {
+    return {
+      loading: true,
+      error: false,
+      song: {
+        name: '',
+        artist: '',
+        writer: '',
+        description: '',
+        imageUrl: '',
+        runtime: null,
+        release: null,
+        lyrics: {
+          orig: {
+            lang: 'Punjabi',
+            lines: [],
+          },
+          translated: {
+            lang: 'English',
+            lines: [],
+          },
+        },
+      },
+    };
+  },
+  computed: {
+    runtime() {
+      const sec = this.song.runtime % 60;
+      const min = (this.song.runtime - sec) / 60;
+      return `${min}:${sec}`;
+    },
+    releaseDate() {
+      if (!this.song.release) return 'N/A';
+      return moment(this.song.release).format('DD MMM, YYYY');
+    },
+  },
+  created() {
+    Meteor.call('findSong', this.$route.params.id, (err, song) => {
+      this.loading = false;
+      if (err) {
+        this.error = true;
+        return;
+      }
+      this.song = song;
+    });
+  },
   methods: {
     loremIpsum,
   },
