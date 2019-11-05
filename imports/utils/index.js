cac5c55f20485e66e6192d60420a0847eca8cd34
Lavneet <lavneetpuri@gmail.com>
2019-11-05 04:35:57 +0000

    i8-fix release date not displaying on ui

--- imports/utils/index.js
+++ imports/utils/index.js
@@ -10,7 +10,7 @@ function generateSong({ numLines }) {
     artist: loremIpsum({ count: randNum(2, 3), units: 'words' }),
     writer: loremIpsum({ count: randNum(2, 3), units: 'words' }),
     runtime: randNum(150, 300),
-    releaseDate: new Date(`${randNum(1, 12)}/${randNum(1, 30)}/${randNum(1998, 2019)}`),
+    release: new Date(`${randNum(1, 12)}/${randNum(1, 30)}/${randNum(1998, 2019)}`),
     description: loremIpsum({ count: randNum(1, 3) }),
     imageUrl: 'https://picsum.photos/1440/400',
     lyrics: {
